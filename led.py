1bccbb8a3dc8ea6440c50ca1d0f45a37bda7baba
tanisho <bsb12solid@gmail.com>
2018-07-01 07:08:11 +0000

    new git

---
+++ led.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)  # ピン番号で指定
+GPIO.setup(22, GPIO.OUT)# 第一引数にGPIOの出力を設定
+
+print("点灯")
+
+for i in range(3):  
+    GPIO.output(22, GPIO.HIGH) # highが点灯、low消灯
+    time.sleep(0.5) # この間は点灯し続ける
+    GPIO.output(22, GPIO.LOW) 
+    time.sleep(0.5) # この間は消滅し続ける
+
+GPIO.cleanup() # GPIOをクリーンアップする
+
